9b4c1c6ea49675eda383db38bf4004396bdf69bc
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2024-07-27 13:38:46 -0400

    minor update

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 
 // Initialize the client
 const stack = new StackClient({
-  // Get your API key and point system id from the Stack dashboard (stack.so)
   apiKey: process.env.STACK_API_KEY || "",
   pointSystemId: 2839,
 });
