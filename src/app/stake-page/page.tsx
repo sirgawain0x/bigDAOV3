907be78c9cf3ffad2fbbf8a24dbe9e8886bcc183
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2024-09-03 12:02:30 -0400

    Update Staking Page
    - Updates to staking and Navbar.

--- src/app/stake-page/page.tsx
+++ src/app/stake-page/page.tsx
@@ -2,11 +2,15 @@ import { Staking } from "@/components/staking/Staking";
 
 export default function StakePage() {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <h1 style={{ marginBottom: "20px" }} className="text-6xl">
-        Stake Rewards
-      </h1>
-      <Staking />
+    <div className="flex flex-col min-h-screen items-center justify-center overflow-x-hidden">
+      <header className="flex flex-col items-center justify-center w-full py-4">
+        <div className="mx-auto">
+          <h1 className="text-3xl mb-5 text-center">Stake Rewards</h1>
+        </div>
+      </header>
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center">
+        <Staking />
+      </main>
     </div>
   );
 }
